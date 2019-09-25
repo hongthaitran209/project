f907472f71b306413331c34fc904eec673fb436c
vietlong <chauvietlong96@gmail.com>
2019-09-25 20:34:31 +0700

    long3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ import { connect } from 'react-redux';
 import { getCourses } from './Actions/Course';
 import CourseList from './Components/CourseList/CourseList';
 import SignIn from './Components/SignIn/SignIn';
-
+import Awnsome from './Components/Awnsome/Awnsome';
+import LearningZone from './Components/learningZone/LearningZone';
+import GetCoaching from './Components/GetCoaching/GetCoaching';
+import Falcuties from './Components/Falcuties/Falcuties';
+import Linked from './Components/Linked/Linked';
+import Footer from './Components/Footer/Footer';
 
 class App extends React.Component {
 
@@ -22,6 +27,12 @@ class App extends React.Component {
         <Carousel />
         <Announcements />
         <CourseList />
+        <Awnsome/>
+        <LearningZone/>
+        <GetCoaching/>
+        <Falcuties/>
+        <Linked/>
+        <Footer/>
       </div>
     );
   }
